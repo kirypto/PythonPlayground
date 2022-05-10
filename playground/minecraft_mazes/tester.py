a3f69bee24b318e9901d8a76d8f2f64548eb74c3
kirypto <kirypto@gmail.com>
2022-05-09 22:01:58 -0600

    Add pretty print option to tester

--- playground/minecraft_mazes/tester.py
+++ playground/minecraft_mazes/tester.py
@@ -1,12 +1,15 @@
 from argparse import ArgumentParser
 from pathlib import Path
 from subprocess import Popen, PIPE, STDOUT
+from typing import List
 
 
 def _main() -> None:
     parser = ArgumentParser(description="Solves the Minecraft maze problem")
     parser.add_argument("input", help="name of the input file")
     parser.add_argument("--output", required=False, help="optional name of the output file")
+    parser.add_argument("--pretty", required=False, action="store_true",
+                        help="print the visual path as text. cannot be combined with '--output'")
     args = parser.parse_args()
 
     input_lines = Path(args.input).read_text("utf8")
@@ -24,6 +27,30 @@ def _main() -> None:
             print("----- Expected:")
             print(expected_result)
             print("-----")
+    elif args.pretty:
+        print(result)
+        print("----- Path: ('^' = start, '$' = end, '*' = path)")
+        lines = input_lines.split()
+        rows = int(lines[0])
+        cols = int(lines[1])
+        pretty_output: List[List[str]] = [[" " for _ in range(cols)] for _ in range(rows)]
+        row, col = [int(char) for char in lines[-4:-2]]
+        pretty_output[row][col] = "^"
+        for move in result.splitlines():
+            if move == "south":
+                row += 1
+            elif move == "north":
+                row -= 1
+            elif move == "east":
+                col += 1
+            elif move == "west":
+                col -= 1
+            else:
+                break
+            pretty_output[row][col] = "*"
+        pretty_output[row][col] = "$"
+        for pretty_row in pretty_output:
+            print(" ".join(pretty_row))
     else:
         print("----- Result:")
         print(result)
