df29ffebcfd163878acaef8060e8d65d754e39b1
kirypto <kirypto@gmail.com>
2022-05-13 15:44:52 -0600

    Modify fibonacci.py to accept the desired number via command line argument or stdin

--- playground/parallel/fibonacci.py
+++ playground/parallel/fibonacci.py
@@ -1,3 +1,10 @@
+"""
+Fibonacci number calculator module.
+- The 'fibonacci' function can be imported from this module and used directly.
+- A positive integer can be provided as a command line argument.
+- If no numeric argument is provided, a single numeric argument will be read from stdin.
+- In both of the above 2 cases, the corresponding fibonacci number will be calculated and printed to stdout.
+"""
 from sys import argv
 
 
@@ -13,10 +20,16 @@ def fibonacci(fibonacci_number: int) -> int:
 
 
 def _main():
-    if len(argv) != 2 or not argv[1].isnumeric():
-        raise ValueError("Must provide exactly 1 integer argument.")
-    fib_num = int(argv[1])
-    print(fibonacci(fib_num))
+    if len(argv) == 1:
+        fib_num_raw = input()
+    elif len(argv) == 2:
+        fib_num_raw = argv[1]
+    else:
+        raise ValueError(f"Exactly 1 integer argument must be provided, but {len(argv) - 1} were.")
+    if not fib_num_raw.isnumeric():
+        raise ValueError(f"Must provide an integer argument, was {fib_num_raw}.")
+
+    print(fibonacci(int(fib_num_raw)))
 
 
 if __name__ == "__main__":
